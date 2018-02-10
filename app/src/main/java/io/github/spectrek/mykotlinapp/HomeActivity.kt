18ed2470fd06bd22a54da10ff0c7794ee12d4579
spectre <gspectre@gmail.com>
2018-02-10 16:27:04 +0300

    Changed greeting text.

--- app/src/main/java/io/github/spectrek/mykotlinapp/HomeActivity.kt
+++ app/src/main/java/io/github/spectrek/mykotlinapp/HomeActivity.kt
@@ -1,12 +1,16 @@
 package io.github.spectrek.mykotlinapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val txtGreeting: TextView = findViewById(R.id.greeting)
+        txtGreeting.text = "Hello everyone!"
     }
 }
